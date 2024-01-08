ce98a4e256ea28a00910319c5091df514e4f99fc
Storm <stormurbjorn24@gmail.com>
2024-01-08 11:21:19 +0700

    new

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # math-foundations-programming
 
-## Exercise 1: find_root.py
+### Exercise 1: find_root.py
+![Image Description](assets/Screenshot 2024-01-08 at 11.17.59.png)
+#### Result: 0.348660945892334
--- find_root.py
+++ find_root.py
@@ -1,5 +1,6 @@
+import math
 
-def find_root_function(f, a, b, max_iterations=1000):
+def find_root_function(f, a, b, max_iterations=1000, eps=1e-6):
 
     if f(a) * f(b) >= 0:
         print("The function must have different signs at a and b")
@@ -8,7 +9,7 @@ def find_root_function(f, a, b, max_iterations=1000):
 
         c = (a + b) / 2
 
-        if f(c) == 0:
+        if abs(f(c)) < eps:
             return c
 
         if f(a) * f(c) < 0:
@@ -16,24 +17,22 @@ def find_root_function(f, a, b, max_iterations=1000):
         else:
             a = c
 
+        print(f"Current value: {f(c)}")
+        print(f"Current interval: [{a}, {b}]")
         print(f"Current interval: {c}")
-        print(f"current iteration: {1000 - el}")
+        print(f"Current iteration: {el} \n")
 
     return (a + b) / 2
 
+# def example_function(x):
+#     return x**2 - 4
 
-
-def example_function(x):
-    return x**2 - 4
-
-def example_function_2(x):
-    return x**3 - 3*x + 1
-
+# def example_function_2(x):
+#     return x**3 - 3*x + 1
 
 def example_function_3(x):
 
     return x**4 - 5*x**3 + 2*x**2 - 3*x + 1
-
 
 root = find_root_function(example_function_3, -4, 4)
 print(f"Groot: {root}")
