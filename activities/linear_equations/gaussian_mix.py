93a9a4efc83718305801896207ec1f7ee757fc15
Storm <stormurbjorn24@gmail.com>
2024-01-11 19:33:26 +0700

    done

--- activities/linear_equations/gaussian_mix.py
+++ activities/linear_equations/gaussian_mix.py
@@ -5,6 +5,8 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from sympy import symbols, diff, sympify
+import re 
+
 
 
 gaussian_array = [[1, 1, 1, 3], [2, -1, -1, 0], [6, -4, 2, 4]]
@@ -61,4 +63,61 @@ def gaussian_elimination(gaussian_array):
         print(f"= {constant_array[i]}")
 
 
-gaussian_elimination(gaussian_array_2)
+    x = [0 for i in range(len(coefficient_array))]
+
+    for i in range(len(coefficient_array)-1, -1, -1):
+
+
+        if coefficient_array[i][i] != 0:
+            x[i] = constant_array[i] / coefficient_array[i][i]
+        else:
+            x[i] = max(constant_array[i], coefficient_array[i][i])
+
+        for k in range(i-1, -1, -1):
+            constant_array[k] -= coefficient_array[k][i] * x[i]
+
+
+
+    print(f"final values: {x}")
+
+
+def convert_to_gaussian_array_corrected(equations):
+    gaussian_array = []
+
+    for eq in equations:
+        parts = eq.split()
+
+        row = []
+
+        sign = 1
+
+        for part in parts:
+            if part == '+':
+                sign = 1
+            elif part == '-':
+                sign = -1
+            elif part == '=':
+                sign = 1
+            else:
+                if 'x' in part:
+                    if part[0] == 'x':
+                        coef = 1
+                    else:
+                        coef = int(part.split('x')[0])
+
+                    row.append(coef * sign)
+                else:
+                    row.append(int(part) * sign)
+
+        gaussian_array.append(row)
+
+    return gaussian_array
+
+
+equation_array = ["x1 + x2 + x3 = 3", "2x1 - x2 - x3 = 0", "6x1 - 4x2 + 2x3 = 4"]
+gaussian_array_result = convert_to_gaussian_array_corrected(equation_array)
+gaussian_elimination(gaussian_array_result)
+
+
+
+# gaussian_elimination(gaussian_array)
